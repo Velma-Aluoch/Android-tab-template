df19a9f97586aabeeb367390d5fa64bc3a70e833
Velma-Aluoch <88651947+Velma-Aluoch@users.noreply.github.com>
2021-10-21 14:26:13 +0300

    Create productFragment.kt

---
+++ productFragment.kt
@@ -0,0 +1,31 @@
+package com.modcom.mytabs.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.modcom.mytabs.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProductFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProductFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+
+
+}
